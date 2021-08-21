f441979fbfea21864148560bccb3068b5c233e99
afshaiqbal <88200881+afshaiqbal@users.noreply.github.com>
2021-08-22 03:04:46 +0530

    Update popup.js

--- src/components/popup.js
+++ src/components/popup.js
@@ -3,19 +3,19 @@ import React from 'react';
 const Popup=()=>{
     return(
         <>
-        <div class="modal hidden">
+        <div class="modal-hidden">
         <button class="close-modal">&times;</button>
         <h1 className="heading">Success</h1>
         
-        <p>
-         Your room has been booked successfully.
+        <p className="success">
+         Your room has been booked<br></br> 
+         successfully.
         </p>
 
-        <h5>Your room details are as follows:</h5>
-        <h6>Hostel No.</h6>
-        <h6>Room No.</h6>
+        <h4>Your room details are as follows:</h4>
+        <h5>Hostel No.G1</h5>
+        <h5>Room No.7</h5>
       </div>
-      <div class="overlay hidden"></div>
       </>
     );
 }

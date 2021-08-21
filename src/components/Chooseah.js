62d64690d9512d6a2946a609d132f6ff0fabb8fb
afshaiqbal <88200881+afshaiqbal@users.noreply.github.com>
2021-08-22 03:01:59 +0530

    Update Chooseah.js

--- src/components/Chooseah.js
+++ src/components/Chooseah.js
@@ -1,34 +1,39 @@
-import React from 'react';
+import React from "react";
 
-const Chooseah=()=>{
-    return(
-        <>
-        <h1 className="heading-image">Choose A Hostel</h1>
-        <div className="buttons">
+const Chooseah = () => {
+  const abc = (e) => {
+    console.log(e, e.target);
+  };
+  return (
+    <>
+      <h1 className="heading-image">Choose A Hostel</h1>
+      <div className="buttons">
         <div>
-            <p className="button">B1</p>
-            <p className="button">B2</p>
-            <p className="button">B3</p>
-            </div>
-            <div>
-            <p className="button">B4</p>
-            <p className="button">B5</p>
-            <p className="button">B6</p>
+          <p className="button">B1</p>
+          <p className="button">B2</p>
+          <p className="button">B3</p>
         </div>
-        </div>
-        <div className="buttons">
         <div>
-            <p className="button">G1</p>
-            <p className="button">G2</p>
-            <p className="button">G3</p>
-            </div>
-            <div>
-            <p className="button">G4</p>
-            <p className="button">G5</p>
-            <p className="button">G6</p>
+          <p className="button">B4</p>
+          <p className="button">B5</p>
+          <p className="button">B6</p>
         </div>
+      </div>
+      <div className="buttons">
+        <div>
+          <p className="button" value="g1" onClick={abc}>
+            G1
+          </p>
+          <p className="button">G2</p>
+          <p className="button">G3</p>
         </div>
-        </>
-    );
-}
+        <div>
+          <p className="button">G4</p>
+          <p className="button">G5</p>
+          <p className="button">G6</p>
+        </div>
+      </div>
+    </>
+  );
+};
 export default Chooseah;
